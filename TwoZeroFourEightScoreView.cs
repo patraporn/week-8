b29b89e9567ebcd8f7ec0b2aa91ac69607412449
Patraporn <rwdnun@hotmail.com>
2015-10-23 18:23:17 +0700

    Homework

---
+++ TwoZeroFourEightScoreView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace twozerofoureight
+{
+    public partial class TwoZeroFourEightScoreView : Form
+    {
+        public TwoZeroFourEightScoreView()
+        {
+            InitializeComponent();
+        }
+        private void lblScore_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void Score_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void TwoZeroFourEightScoreView_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
